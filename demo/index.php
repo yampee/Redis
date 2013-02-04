dbb36ebf2798130df9ea9a79e598bd5a0bd87a8f
Titouan Galopin <galopintitouan@gmail.com>
2013-02-04 17:34:39 +0100

    Update demo to add all possibilities.

--- demo/index.php
+++ demo/index.php
@@ -4,8 +4,36 @@ require '../autoloader.php';
 
 $redis = new Yampee_Redis_Client();
 
+// Getters/Setters
 $redis->set('key', 'value');
 
 if ($redis->has('key')) {
 	$redis->get('key');
 }
+
+$redis->remove('key');
+
+// You can authenticate on the server:
+$redis->authenticate('password');
+
+// You can remove an expiration on a value
+$redis->persist('key');
+
+// You can find keys that match a given pattern
+$redis->findKeys('pattern');
+
+// You can flush the database (delete all keys)
+$redis->flush();
+
+// You can get server stats
+$redis->getStats();
+
+// You can configure the server
+$redis->getParameter('parameterName');
+$redis->setParameter('parameterName', 'parameterValue');
+
+// You can get the database size
+$redis->getSize();
+
+// And for each more specific command, you can use send()
+$redis->send($command, $arguments);
